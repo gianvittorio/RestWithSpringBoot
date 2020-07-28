9a05f1c10c240fd3cae51e3d69907455c9429469
gianvittorio <gianvi13@hotmail.com>
2020-07-28 22:41:36 +0200

    done with greetings rest

--- rest-with-spring-boot/src/main/java/br/com/gianvi/RestWithSpringBootApplication.java
+++ rest-with-spring-boot/src/main/java/br/com/gianvi/RestWithSpringBootApplication.java
@@ -1,9 +1,11 @@
-package br.com.gianvittorio.demo;
+package br.com.gianvi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("br.com.gianvi")
 public class RestWithSpringBootApplication {
 
 	public static void main(String[] args) {
--- rest-with-spring-boot/src/test/java/br/com/gianvi/RestWithSpringBootApplicationTests.java
+++ rest-with-spring-boot/src/test/java/br/com/gianvi/RestWithSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.gianvittorio.demo;
+package br.com.gianvi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
